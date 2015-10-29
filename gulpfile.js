e25aa7d935c1225e53cdbd611c47b225ec6db840
David Paulsson <david@davidpaulsson.se>
2015-10-29 13:39:08 +0100

    Optimize images
    Saved 283,5 KB on 5 images

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var svgmin = require('gulp-svgmin');
 var svgstore = require('gulp-svgstore');
+var imageop = require('gulp-image-optimization');
 
 gulp.task('svg', function () {
   return gulp
@@ -23,5 +24,16 @@ gulp.task('svg:minify', function () {
   return gulp
     .src('img/*.svg')
     .pipe(svgmin())
+    .pipe(gulp.dest('img'));
+});
+
+gulp.task('img', function () {
+  return gulp
+    .src(['img/*.png','img/*.jpg','img/*.gif','img/*.jpeg'])
+    .pipe(imageop({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+    }))
     .pipe(gulp.dest('img'));
 });
